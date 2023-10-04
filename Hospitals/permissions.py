66ba07df28e275197934e810b5e669b045148103
vivekjha1213 <67068290+vivekjha1213@users.noreply.github.com>
2023-10-04 18:37:49 +0530

    Untrack ignored files and directories

--- Hospitals/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import permissions
-
-class UnrestrictedPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return True  # Allow unrestricted access
