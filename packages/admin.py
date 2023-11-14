48457b2a3537caaf592bf13f0c4c7edb712f1dc5
vivekjha1213 <67068290+vivekjha1213@users.noreply.github.com>
2023-11-14 23:58:08 +0530

    Added more interface

--- packages/admin.py
+++ packages/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Package
 
-# Register your models here.
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = (
+        "package_name",
+        "client",
+        "current_package",
+        "description",
+        "monthly_price",
+        "yearly_price",
+        "start_date",
+        "end_date",
+    )
+    list_filter = ("client",) 
+    search_fields = ("package_name", "description", "client__name") 
+    ordering = ("-start_date",) 
